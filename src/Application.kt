ec2ba6811c18fc7cc5f5f5193b86e73209f399f7
Marc Tatham <marctatham@gmail.com>
2020-04-14 12:24:36 +0100

    provide a simple "hello world" response

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,22 @@
 package com.marctatham
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.jetty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    routing {
+        get("/") {
+            call.respondText("hello world", contentType = ContentType.Text.Plain)
+        }
+    }
 }
 
