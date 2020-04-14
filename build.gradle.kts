960c02ef286010626f49cb0fbbcf7c9ec5d6c8d0
Marc Tatham <marctatham@gmail.com>
2020-04-14 15:45:45 +0100

    Added suppport for building UberJar
     - added gradle plugin: "shadow"
     - configured shadowJar task to properly build the UberJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -8,6 +9,9 @@ val versionLogback: String by project
 plugins {
     application
     kotlin("jvm") version "1.3.70"
+
+    // Shadow plugin - enable support for building our UberJar
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 group = "com.marctatham"
@@ -36,3 +40,23 @@ kotlin.sourceSets["test"].kotlin.srcDirs("test")
 
 sourceSets["main"].resources.srcDirs("resources")
 sourceSets["test"].resources.srcDirs("testresources")
+
+// Configure the "shadowJar" task to properly build our UberJar
+// we effectively want a jar with zero dependencies we can run and will "just work"
+tasks {
+    named<ShadowJar>("shadowJar") {
+        // Appends entries in META-INF/services resources into a single resource. For example, if there are several
+        // META-INF/services/org.apache.maven.project.ProjectBuilder resources spread across many JARs the individual
+        // entries will all be concatenated into a single META-INF/services/org.apache.maven.project.ProjectBuilder
+        // resource packaged into the resultant JAR produced by the shading process -
+        // Effectively ensures we bring along all the necessary bits from Jetty
+        mergeServiceFiles()
+
+        // As per the App Engine java11 standard environment requirements listed here:
+        // https://cloud.google.com/appengine/docs/standard/java11/runtime
+        // Your Jar must contain a Main-Class entry in its META-INF/MANIFEST.MF metadata file
+        manifest {
+            attributes(mapOf("Main-Class" to application.mainClassName))
+        }
+    }
+}
