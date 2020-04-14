7bdf1d1c2dfc03fdfc3b02c5688dd5aa2cc321a5
Marc Tatham <marctatham@gmail.com>
2020-04-14 12:18:38 +0100

    New Ktor Project creation
    This project was created using the intellij plugin for Ktor.
     - Gradle Kotlin DSL,
     - Jetty and
     - Ktor version 1.3.2
    
    Intellij Plugin:
    https://plugins.jetbrains.com/plugin/10823-ktor

---
+++ src/Application.kt
@@ -0,0 +1,13 @@
+package com.marctatham
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.jetty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+}
+
